2374ab360250d448bd278054f287feacebc56c51
Andrea Buttarelli <andreabuttarelli@outlook.com>
2021-01-12 16:24:23 +0100

    12/01/2021

--- eterofobia/lib/src/pages/home/row_data.dart
+++ eterofobia/lib/src/pages/home/row_data.dart
@@ -5,78 +5,116 @@ import 'package:google_fonts/google_fonts.dart';
 class RowData extends StatelessWidget {
   final String title;
   final String value;
-  final Color color;
+  final List<Color> colors;
   const RowData({
     Key key,
     this.title,
     this.value,
-    this.color,
+    this.colors,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Wrap(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(right: 16),
-                  child: Text(
+      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+      child: Container(
+        width: (MediaQuery.of(context).size.width > 1000)
+            ? 1000
+            : MediaQuery.of(context).size.width,
+        height: 300,
+        padding: const EdgeInsets.only(top: 40),
+        decoration: BoxDecoration(
+          color: Colors.white12,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Wrap(
+                children: [
+                  Text(
                     title,
-                    style: GoogleFonts.manrope(
+                    style: GoogleFonts.poppins(
                       color: Colors.white,
-                      fontSize: 16,
+                      fontSize: 22,
                       letterSpacing: 1,
-                      fontWeight: FontWeight.normal,
+                      fontWeight: FontWeight.bold,
                     ),
-                    textAlign: TextAlign.left,
+                    textAlign: TextAlign.center,
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Container(
-            width: 40,
-            height: 40,
-            child: Stack(
-              children: [
-                Blob.animatedRandom(
-                  size: 40,
-                  loop: true,
-                  duration: Duration(milliseconds: 2000),
-                  styles: BlobStyles(),
-                ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          value,
-                          style: GoogleFonts.manrope(
-                            color: Colors.white,
-                            fontSize: 22,
-                            letterSpacing: 1,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.right,
+            Center(
+              child: Container(
+                width: (MediaQuery.of(context).size.width > 1000)
+                    ? 500
+                    : MediaQuery.of(context).size.width - 48,
+                child: Stack(
+                  children: [
+                    Blob.animatedRandom(
+                      size: (MediaQuery.of(context).size.width > 1000)
+                          ? 500
+                          : MediaQuery.of(context).size.width - 48,
+                      loop: true,
+                      duration: Duration(milliseconds: 2000),
+                      styles: BlobStyles(
+                        color: colors[0],
+                      ),
+                    ),
+                    Blob.animatedRandom(
+                      size: (MediaQuery.of(context).size.width > 1000)
+                          ? 500
+                          : MediaQuery.of(context).size.width - 48,
+                      loop: true,
+                      duration: Duration(milliseconds: 2000),
+                      styles: BlobStyles(
+                        color: colors[1],
+                        fillType: BlobFillType.stroke,
+                        strokeWidth: 12,
+                      ),
+                    ),
+                    Blob.animatedRandom(
+                      size: (MediaQuery.of(context).size.width > 1000)
+                          ? 500
+                          : MediaQuery.of(context).size.width - 48,
+                      loop: true,
+                      duration: Duration(milliseconds: 2000),
+                      styles: BlobStyles(
+                        color: colors[2],
+                        fillType: BlobFillType.stroke,
+                        strokeWidth: 12,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: (MediaQuery.of(context).size.width > 1000)
+                              ? (500 / 2 - 120)
+                              : MediaQuery.of(context).size.width / 2.5 - 120),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "0",
+                              style: GoogleFonts.ibmPlexSans(
+                                color: Colors.white,
+                                fontSize: 180,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
